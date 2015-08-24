b7fab900bc261fbfb06d8cc50e4e498162afb7aa
Tom Small <tom@offerpop.com>
2015-08-24 18:13:50 -0400

    example(globals): create implicit global variable examples

--- globals.js
+++ globals.js
@@ -1,15 +1,72 @@
-"use strict";
-
-
 let GlobalHelpers = {
+    getPeople: () => [
+        {
+            name: 'John Doe',
+            siblings: ['Jack', 'Jane']
+        },
+        {
+            name: 'Jack Doe',
+            siblings: ['John', 'Jane']
+        }
+    ],
+
+    getFix: () => '"use strict;"'
 };
 
 let GlobalExamples = [
     {
-        title: "Example Data",
+        title: "Implicit Globals (Bad)",
         run: (logger) => {
-            logger("Hello world.");
+            function describePeople(people) {
+                for (i = 0; i < people.length; i++) {
+                    describePerson(people[i]);
+                }
+            }
+
+            function describePerson(person) {
+                var description = [person.name, 'is related to'];
+                var siblings = [];
+                for (i = 0; i < person.siblings.length; i++) {
+                    siblings.push(person.siblings[i]);
+                }
+                description.push(siblings.join(', '));
+                logger(description.join(' '));
+            }
+
+            describePeople(GlobalHelpers.getPeople());
         }
+    },
+    {
+        title: "Implicit Globals (Better)",
+        run: (logger) => {
+            function describePeople(people) {
+                var i;
+                for (i = 0; i < people.length; i++) {
+                    describePerson(people[i]);
+                }
+            }
+
+            function describePerson(person) {
+                var description = [person.name, 'is related to'];
+                var siblings = [];
+                var i;
+                for (i = 0; i < person.siblings.length; i++) {
+                    siblings.push(person.siblings[i]);
+                }
+                description.push(siblings.join(', '));
+                logger(description.join(' '));
+            }
+
+            describePeople(GlobalHelpers.getPeople());
+        }
+    
+    },
+    {
+        title: "Implicit Globals (Best)",
+        run: (logger) => {
+            logger(GlobalHelpers.getFix());
+        }
+    
     }
 ];
 
