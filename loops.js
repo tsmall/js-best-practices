4cf468c2fc0b923acd25057a66b9f6729e8122e4
Tom Small <tom@offerpop.com>
2015-08-24 23:42:12 -0400

    example(loops): create looping and iteration examples

---
+++ loops.js
@@ -0,0 +1,81 @@
+"use strict";
+
+let LoopHelpers = {};
+
+let LoopExamples = [
+    {
+        title: "Arrays: for...in",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            // people.foo = 'bar';
+            for (var each in people) {
+                logger(each);
+            }
+        }
+    },
+    {
+        title: "Arrays: for...of",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            // people.foo = 'bar';
+            for (var each of people) {
+                logger(each);
+            }
+        }
+    },
+    {
+        title: "Arrays: forEach",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            people.forEach(each => logger(each));
+            // people.forEach(logger);
+        }
+    },
+    {
+        title: "Arrays: Convert to Uppercase (for...of)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            var lowercasePeople = [];
+            for (var name of people) {
+                lowercasePeople.push(name.toUpperCase());
+            }
+            logger(lowercasePeople);
+        }
+    },
+    {
+        title: "Arrays: Convert to Uppercase (map)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            const lowercasePeople = people.map(name => name.toUpperCase());
+            logger(lowercasePeople);
+        }
+    },
+    {
+        title: "Arrays: Find 'A' Names (for...of)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            var aNames = [];
+            for (var name of people) {
+                if (name.includes('a')) {
+                    aNames.push(name);
+                }
+            }
+            logger(aNames);
+        }
+    },
+    {
+        title: "Arrays: Find 'A' Names (for...of)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            const aNames = people.filter(name => name.includes('a'));
+            logger(aNames);
+        }
+    }
+];
+
+Registrar.register({
+    title: 'Looping',
+    helpers: LoopHelpers,
+    helpersName: 'LoopHelpers',
+    examples: LoopExamples
+});
