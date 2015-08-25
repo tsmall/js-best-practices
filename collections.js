bdb2c759815f099e5a7fc7c03245b7ab454941d9
Tom Small <tom@offerpop.com>
2015-08-24 23:43:06 -0400

    refactor(collections): tweak the collection examples

--- collections.js
+++ collections.js
@@ -1,5 +1,9 @@
 let CollectionsHelpers = {
 
+    Person: function Person(name) {
+        this.name = name;
+    },
+
     logSet: function(logger, set) {
         if (set instanceof Set) {
             this.logRealSet(logger, set);
@@ -54,52 +58,58 @@ let CollectionsExamples = [
         }
     },
     {
-        title: 'Objects: Like Maps',
+        title: 'Objects: Fake Maps',
         run: (logger) => {
-            var fakeDict = {
+            var fakeMap = {
                 first: 'John',
                 last: 'Doe'
             };
-            logger("First Name: " + fakeDict['first']);
-            logger("Last Name: " + fakeDict['last']);
+            logger("First Name: " + fakeMap['first']);
+            logger("Last Name: " + fakeMap['last']);
         }
     },
     {
-        title: 'Objects: Non-String Keys',
+        title: 'Objects: Non-String Keys (Part 1)',
         run: (logger) => {
-            function Person(name) {
-                this.name = name;
-            }
+            var john = new CollectionsHelpers.Person('John Doe');
+            var jack = new CollectionsHelpers.Person('Jack Doe');
 
-            var john = new Person('John Doe');
-            var jack = new Person('Jack Doe');
-
-            var fakeDict = {
-                [john]: john.name,
-                [jack]: jack.name
+            var fakeMap = {
+                john: john.name,
+                jack: jack.name
             };
 
-            logger("john: " + fakeDict[john]);
-            logger("jack: " + fakeDict[jack]);
+            logger("john: " + fakeMap[john]);
+            logger("jack: " + fakeMap[jack]);
         }
     },
     {
-        title: 'Maps',
+        title: 'Objects: Non-String Keys (Part 2)',
         run: (logger) => {
-            function Person(name) {
-                this.name = name;
-            }
+            var john = new CollectionsHelpers.Person('John Doe');
+            var jack = new CollectionsHelpers.Person('Jack Doe');
 
-            var john = new Person('John Doe');
-            var jack = new Person('Jack Doe');
+            var fakeMap = {};
+            fakeMap[john] = john.name;
+            fakeMap[jack] = jack.name;
 
-            var fakeDict = new Map([
+            logger("john: " + fakeMap[john]);
+            logger("jack: " + fakeMap[jack]);
+        }
+    },
+    {
+        title: 'Maps: Real Maps',
+        run: (logger) => {
+            var john = new CollectionsHelpers.Person('John Doe');
+            var jack = new CollectionsHelpers.Person('Jack Doe');
+
+            var map = new Map([
                 [john, john.name],
                 [jack, jack.name]
             ]);
 
-            logger("john: " + fakeDict.get(john));
-            logger("jack: " + fakeDict.get(jack));
+            logger("john: " + map.get(john));
+            logger("jack: " + map.get(jack));
         }
     },
     {
@@ -130,14 +140,10 @@ let CollectionsExamples = [
     {
         title: 'Objects: Sets of Objects',
         run: (logger) => {
-            function Person(name) {
-                this.name = name;
-            }
-
-            var john = new Person('John Doe');
-            var jack = new Person('Jack Doe');
-            var jane = new Person('Jane Doe');
-            var jess = new Person('Jane Doe');
+            var john = new CollectionsHelpers.Person('John Doe');
+            var jack = new CollectionsHelpers.Person('Jack Doe');
+            var jane = new CollectionsHelpers.Person('Jane Doe');
+            var jess = new CollectionsHelpers.Person('Jane Doe');
 
             var fakeSet = {
                 [john]: null,
