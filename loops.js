60e1fe9bf34eac7abd9c8c6b4be94514b82343c5
Tom Small <tom@offerpop.com>
2015-08-25 17:47:58 -0400

    example(loops): add more looping examples

--- loops.js
+++ loops.js
@@ -64,11 +64,75 @@ let LoopExamples = [
         }
     },
     {
-        title: "Arrays: Find 'A' Names (for...of)",
+        title: "Arrays: Find 'A' Names (filter)",
         run: (logger) => {
             const people = ['john', 'jack', 'jane'];
             const aNames = people.filter(name => name.includes('a'));
             logger(aNames);
+        }
+    },
+    {
+        title: "Arrays: Join (for...of)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            let allNames = '';
+            for (let name of people) {
+                allNames += `${name} `;
+            }
+            logger(allNames);
+        }
+    },
+    {
+        title: "Arrays: Join (reduce)",
+        run: (logger) => {
+            const people = ['john', 'jack', 'jane'];
+            const allNames = people.reduce(
+                (result, name) => result + `${name} `,
+                ''
+            );
+            logger(allNames);
+        }
+    },
+    {
+        title: "Maps: for...of (keys)",
+        run: (logger) => {
+            const ages = new Map([
+               ['John', 21],
+               ['Jane', 22],
+               ['Jack', 23]
+            ]);
+            
+            for (let key of ages.keys()) {
+                logger(key);
+            }
+        }
+    },
+    {
+        title: "Maps: for...of (values)",
+        run: (logger) => {
+            const ages = new Map([
+               ['John', 21],
+               ['Jane', 22],
+               ['Jack', 23]
+            ]);
+            
+            for (let value of ages.values()) {
+                logger(value);
+            }
+        }
+    },
+    {
+        title: "Maps: for...of (entries)",
+        run: (logger) => {
+            const ages = new Map([
+               ['John', 21],
+               ['Jane', 22],
+               ['Jack', 23]
+            ]);
+            
+            for (let entry of ages.entries()) {
+                logger(`${entry[0]}: ${entry[1]}`);
+            }
         }
     }
 ];
